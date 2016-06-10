a1e1d9ad8865d19d47ed787e924ff5073609a40c
Michael Mead <michaelmead007@icloud.com>
2016-06-10 09:19:16 -0500

    add bootstrap and the ability to nominate for new votes

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-angular.module('app', [])
+angular.module('app', ['angular.filter'])
   .config(() => {
-    firebase.initializeApp({
-       apiKey: "AIzaSyDwW2yfWv13XviFVaanBvM6wI1cix6J1Ts",
+  // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyDwW2yfWv13XviFVaanBvM6wI1cix6J1Ts",
     authDomain: "movie-history-e5c21.firebaseapp.com",
     databaseURL: "https://movie-history-e5c21.firebaseio.com",
     storageBucket: "movie-history-e5c21.appspot.com",
-    })
+    };
+  firebase.initializeApp(config);
   })
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function($timeout) {
     const main = this;
 
-    main.heading = 'Vote Now!!!';
+    main.options = {};
 
-    main.HarleyDavidson = function () {
-      firebase.database().ref('/votes/HarleyDavidson')
-        .set(main.HarleyDavidsonCount + 1)
-    }
-    main.Honda = function () {
-      firebase.database().ref('/votes/Honda')
-        .set(main.HondaCount + 1)
+    main.submitNew = {
+      votes: 0
     }
 
-    firebase.database().ref('/votes').on('value', (change) => {
-      const data = change.val();; // {HarleyDavidson: 1, Honda: 2}
-      console.log("data: ", data);
-      main.HarleyDavidsonCount = data.HarleyDavidson
-      main.HondaCount = data.Honda
-      // $scope.$apply()
+    main.header = 'Vote for anything, just add your nomination below.';
+
+    main.nominateCount = function(choice) {
+      firebase.database().ref(`/votes/${choice.$key}/votes`).set(choice.votes + 1);
+    }
+
+    main.nominate = function(newMovie) {
+      $timeout(() => {
+        firebase.database().ref('/votes').push(newMovie);
+      })
+    }
+
+  // Firebase listeners for child data updates
+    firebase.database().ref('/votes').on('child_added', function(childData) {
+      $timeout(() => {
+        main.options[childData.key] = childData.val();
+      });
     })
-  })  
+
+    firebase.database().ref('/votes').on('child_changed', function(childData) {
+      $timeout(() => {
+        main.options[childData.key] = childData.val();
+      });
+    })
+
+  })
